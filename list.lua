b5c16b3f923fa12f6530c83ff52299d274af79e7
awesomeusername69420 <24661212+awesomeusername69420@users.noreply.github.com>
2023-01-07 02:34:36 -0500

    Create list.lua

---
+++ list.lua
@@ -0,0 +1,169 @@
+SteamIDs = { -- Friends, notable people, and noobs
+	["STEAM_0:1:421735072"] = "wHjKoPXcVBNa", -- Main; aka BensoBenny, ElKingLaJefe, /, slashelement; Discord ID 933007993761591297
+	["STEAM_0:0:48194892"] = "wHjKoPXcVBNa", -- Alt (Old main)
+
+	["STEAM_0:1:501990999"] = "Lucy", -- Main; Original Fun Server owner
+	["STEAM_0:0:212191500"] = "Lucy", -- Alt
+	["STEAM_0:0:501847709"] = "Lucy", -- Alt
+
+	["STEAM_0:1:206450007"] = "Zachkilla", -- Main; Discord ID 732162751689916457
+	["STEAM_0:1:529114618"] = "Zachkilla", -- Alt
+
+	["STEAM_0:0:4727797"] = "NoDataFound", -- Main; Discord ID 968626855542407229, 898028985353650217
+	["STEAM_0:0:586186188"] = "NoDataFound", -- Alt
+
+	["STEAM_0:0:943488"] = "Fraay", -- Main
+	["STEAM_0:0:524267287"] = "Fraay", -- Alt
+	["STEAM_0:1:12522"] = "Fraay", -- Alt
+
+	["STEAM_0:0:530792749"] = "Chef", -- Main
+	["STEAM_0:0:538743441"] = "Chef", -- Alt
+
+	-- Lenn and Libre have some weird account shenanigans going on ( Who needs to ask when you can just guess B] )
+	["STEAM_0:0:453356413"] = "Lenn", -- Main; Discord ID 806251288765005865, 706667030677553238
+	["STEAM_0:0:453849921"] = "Lenn", -- Alt
+	["STEAM_0:1:451372581"] = "Lenn", -- Alt
+	["STEAM_0:1:552841437"] = "Lenn", -- Alt
+	["STEAM_0:1:554600789"] = "Lenn", -- Alt
+	["STEAM_0:1:716039459"] = "Lenn", -- Alt
+	["STEAM_0:1:716016191"] = "Lenn", -- Alt
+	["STEAM_0:1:182201692"] = "Lenn", -- Alt
+
+	["STEAM_0:1:12347527"] = "Sticky", -- Main; aka Libre; Discord ID 259815124502708224, 297946783059804162
+	["STEAM_0:0:449532271"] = "Sticky", -- Alt
+	["STEAM_0:0:451131326"] = "Sticky", -- Alt
+	["STEAM_0:0:451233678"] = "Sticky", -- Alt
+	["STEAM_0:0:67826129"] = "Sticky", -- Alt
+	["STEAM_0:1:39063143"] = "Sticky", -- Alt
+	["STEAM_0:1:449440906"] = "Sticky", -- Alt
+	["STEAM_0:1:451114145"] = "Sticky", -- Alt
+	["STEAM_0:1:453919277"] = "Sticky", -- Alt
+
+	["STEAM_0:1:59437636"] = "MayaStoat", -- Main
+	["STEAM_0:0:517326488"] = "MayaStoat", -- Alt
+
+	["STEAM_0:1:430197362"] = "Asriel", -- Main; Discord ID 936106626585362474
+	["STEAM_0:1:28050194"] = "Asriel", -- Alt
+
+	["STEAM_0:0:63644275"] = "phizz", -- Main; Discord ID 462339679497486347
+	["STEAM_0:0:162667998"] = "phizz", -- Alt
+
+	["STEAM_0:1:27520647"] = "Frzy", -- Main; Discord ID 501197617737236480
+	["STEAM_0:1:526231390"] = "Frzy", -- Alt
+
+	["STEAM_0:1:121865665"] = "Synk", -- Main; aka Jeremy Stencill; Discord ID 197808661937127435
+	["STEAM_0:1:160717043"] = "Synk", -- Alt (Bot account)
+	["STEAM_0:0:695274096"] = "Synk", -- Alt (Bot account)
+	["STEAM_0:1:612846024"] = "Synk", -- Alt (Bot account)
+	["STEAM_0:1:612966849"] = "Synk", -- Alt (Bot account)
+
+	["STEAM_0:1:245470923"] = "Nima (retard)", -- Supposed main; Massive dumbfuck; Discord ID 1053336059259400213
+	["STEAM_0:1:649779878"] = "Nima (retard)", -- Alt (CasheMoney_)
+	["STEAM_0:0:650071121"] = "Nima (retard)", -- Alt (JackSeptic321)
+	["STEAM_0:0:650111217"] = "Nima (retard)", -- Alt (CentChat)
+
+	["STEAM_0:1:82072473"] = "Rose", -- Main; Discord ID 765337493636907049, 961743959263178863, 983607237723455529
+	["STEAM_0:0:502525640"] = "Rose", -- Alt; I had a 3rd one but I've lost it :(
+
+	["STEAM_0:1:547859568"] = "StormyStyx", -- Main; Discord ID 981403816245231636, 557390100045234186, 483455368597471232
+	["STEAM_0:1:625263944"] = "StormyStyx", -- Alt
+
+	["STEAM_0:0:653643904"] = "radiated", -- Main; Not 100% on who this is, characteristics similar to Dababy; Discord ID 1007267376703750184
+	["STEAM_0:0:50172698"] = "radiated", -- Alt
+
+	-- Fancepunchers
+	["STEAM_0:1:7099"] = "Garry", -- The man himself; Discord ID 112179473226469376
+	["STEAM_0:0:18313012"] = "Robotboy655", -- aka Rubat; Discord ID 165597011406618624
+	["STEAM_0:0:140039814"] = "Alistar", -- Discord ID 143672569919307776
+	["STEAM_0:1:72204083"] = "Jake Rich", -- Discord ID 126077584890003456
+	["STEAM_0:0:25853318"] = "Layla", -- aka Laylad, Discord ID 274525414326534145
+	["STEAM_0:1:19779739"] = "Max", -- aka MaxOfS2D; Discord ID 86136790951419904
+	["STEAM_0:0:40642675"] = "ogniK", -- Discord ID 104830539185319936
+
+	-- Nytsense fatsos
+	["STEAM_0:1:609734202"] = "Tabzware (Fake)", -- Wannabe Tabzware; 67.170.139.42 - Portland, Oregon
+	["STEAM_0:0:71256913"] = "Dazed", -- 174.174.14.9 - Beaverton, Oregon; Discord ID 380535512504598529
+	-- Don't have nyt's Steam ID. Discord ID 1035397773622792284, 1035397773622792284, 1059898533945290822
+
+	-- TTN fags
+	["STEAM_0:0:496685686"] = "gopnik", -- 129.222.84.59 - Atlanta, Georgia
+	["STEAM_0:0:527071396"] = "Cyber",
+	["STEAM_0:1:230862410"] = "PxelPrfect", -- 173.93.155.169 - Lexington, South Carolina; Discord ID 1035648799688900649
+	["STEAM_0:0:32052275"] = "Pastgreyakuma", -- aka Pastgrey ( Can't remember if he was a TTN member or was just in their Discord, close enough )
+
+	-- The rest
+	["STEAM_0:0:103884710"] = "slayer key",
+	["STEAM_0:0:11101"] = "Gabe Newell",
+	["STEAM_0:0:111852171"] = "Merio", -- aka MerioMan; Discord ID 929145436030238850, 459728297308454924
+	["STEAM_0:0:126026636"] = "0Exn (pedophile)", -- Loli lover
+	["STEAM_0:0:135363990"] = "Enkai", -- aka Aiko, exi; Discord ID 382320302820163586
+	["STEAM_0:0:139661276"] = "Julie", -- Way too many aliases for me to care about noting down; Enkai's boyfriend; Discord ID 369153012070875139
+	["STEAM_0:0:147989140"] = "Mario Judah", -- Discord ID 528035248865280010
+	["STEAM_0:0:155836946"] = "SamFerrari", -- Discord ID
+	["STEAM_0:0:166343945"] = "Omar",
+	["STEAM_0:0:168107795"] = "Eggapegawsus", -- Discord ID 763596317095493642
+	["STEAM_0:0:170053268"] = "krakenautical", -- Discord ID 473522529001013248
+	["STEAM_0:0:177527067"] = "senator",
+	["STEAM_0:0:177590457"] = "jynxx", -- Discord ID 300382681504677888
+	["STEAM_0:0:191347153"] = "Queen Maffin", -- aka Maffin
+	["STEAM_0:0:196831489"] = "LilTraumatik",
+	["STEAM_0:0:200336136"] = "Asteya", -- Discord ID 293958821498322946
+	["STEAM_0:0:205319780"] = "Exodium", -- Discord ID 310613022517362688
+	["STEAM_0:0:20795098"] = "naughty", -- Discord ID 592599320327487521
+	["STEAM_0:0:215472756"] = "Brutale", -- Discord ID 338561601017741312
+	["STEAM_0:0:217090736"] = "Burger", -- Discord ID 239194736395550720
+	["STEAM_0:0:221374125"] = "justme", -- Discord ID 509474677383495691
+	["STEAM_0:0:228660077"] = "wiLLi3m", -- Discord ID 543951601371578378
+	["STEAM_0:0:235273032"] = "hwc", -- Discord ID 729500896853229580
+	["STEAM_0:0:238414867"] = "Mister Charles", -- Fat idiot; Discord ID 679485933250543627, 824454259256590367
+	["STEAM_0:0:29540342"] = "Iced Coffee", -- Discord ID 176871354979581953
+	["STEAM_0:0:35717190"] = "GrandpaTroll", -- Discord ID 181273408879722496
+	["STEAM_0:0:41908082"] = "homonovus", -- aka James; Discord ID 107827535479353344
+	["STEAM_0:0:436664833"] = "Nikasia", -- aka Callie; Discord ID 440224629768978432
+	["STEAM_0:0:444070083"] = "Daer", -- Discord ID 281926090606379008
+	["STEAM_0:0:456670572"] = "erratic", -- Discord ID 216339220392837130
+	["STEAM_0:0:457"] = "Rak",
+	["STEAM_0:0:47070558"] = "WinterHeartZ", -- aka Tempest
+	["STEAM_0:0:47299591"] = "donkeypounder44", -- Discord ID <REDACTED> :^)
+	["STEAM_0:0:50315727"] = "Fitzganch", -- aka Fitz; Discord ID 347779786002661377
+	["STEAM_0:0:528832741"] = "anane", -- aka ashane; Discord ID 665445699562897430
+	["STEAM_0:0:531680934"] = "M1sfit",
+	["STEAM_0:0:555277374"] = "roseluck", -- aka MOwGli; Discord ID 843624460463898625
+	["STEAM_0:0:564394173"] = "Father of McBeth", -- Discord ID 733026111860244480
+	["STEAM_0:0:564750471"] = "Sai", -- aka Cat; Discord ID 746443985253564576
+	["STEAM_0:0:58188317"] = "prof", -- aka profande
+	["STEAM_0:0:59123135"] = "mkm", -- aka mkmhome; Discord ID 852290566435897384, 746493388555026533, 746158082266234933, 746492133057495071, 746155494590251038, 746497593961349170, 746496880707633153, 746138322891440234, 746491937657192468, 746141152134168738, 746136926662295692, 746157291610308660, 746144415999066212, 746138008507515060, 841381794782773309, 746495131267498064, 746495763231801435, 746496383854444634,746154047689457755, 746143028892270593, 746154337650081864, 746142212651221072, 746145068229984346, 749357147653014009, 746143699964133387, 746154243836084394
+	["STEAM_0:0:599085225"] = "t0ny", -- Discord ID 447175786039083008
+	["STEAM_0:0:64817022"] = "Rooty", -- Discord ID 484646436529504257
+	["STEAM_0:0:656935332"] = "Jacques_n0sw1m",
+	["STEAM_0:0:66543099"] = "Cuervo", -- Le Mexican; Discord ID 871787708727570482
+	["STEAM_0:0:68635133"] = "cash", -- aka c; Discord ID 1033604637334458460
+	["STEAM_0:0:71211372"] = "Elias", -- Discord ID 489518808025792512
+	["STEAM_0:0:74527587"] = "s0lum", -- Noob; Discord ID 791131045168807937
+	["STEAM_0:0:84290845"] = "DonutSandwich01", -- Discord ID 210544305163468800
+	["STEAM_0:0:85055645"] = "Obama Barack", -- Discord ID 847959218920751174
+	["STEAM_0:0:98497976"] = "Octavia", -- Discord ID 227960757751382018
+	["STEAM_0:1:104531418"] = "Cobra", -- aka Cober
+	["STEAM_0:1:118269461"] = "Banksy", -- :O
+	["STEAM_0:1:130219310"] = "Mochi", -- Discord ID 396074796443762688
+	["STEAM_0:1:146653457"] = "Tranquility",
+	["STEAM_0:1:151534266"] = "Citymations", -- Discord ID 209779434998333440
+	["STEAM_0:1:15642011"] = "Pax AU", -- https://www.youtube.com/watch?v=rrnR0FcrPFs
+	["STEAM_0:1:216918676"] = "Mr. Bass", -- aka Aiden; Discord ID 261189993320415232
+	["STEAM_0:1:233081768"] = "namehumor", -- Discord ID 417836731497185280
+	["STEAM_0:1:244495956"] = "Woob", -- Discord ID 858002993685594134
+	["STEAM_0:1:245495767"] = "noob dog", -- aka Boring
+	["STEAM_0:1:84453542"] = "dogemember", -- aka hamond; Discord ID 323095153311219712
+	["STEAM_0:1:437790225"] = "Milk", -- aka Ethan Tomany; Discord ID 769343214217592843
+	["STEAM_0:1:446095209"] = "Axis", -- aka Lund; Discord ID 1002324690271993937
+	["STEAM_0:1:520598481"] = "n0x1ous", -- Kind of fat; Discord ID 584512720540401664
+	["STEAM_0:1:526449367"] = "tod", -- Discord ID 761754613585936416
+	["STEAM_0:1:544775250"] = "Moe", -- Discord ID 729145572237508680
+	["STEAM_0:1:64777074"] = "Weegeeman", -- aka Mr.Weegeeman
+	["STEAM_0:1:66057978"] = "Yogpod", -- 'STEAM_0:0:26573693' Potential alt?; Discord ID 461011076763353090
+	["STEAM_0:1:76873410"] = "HiptonismHD", -- Discord ID 238793587473645578
+	["STEAM_0:1:86419214"] = "WomanSlapper", -- Discord ID 852276933722505258
+	["STEAM_0:1:9592127"] = "BlacK", -- aka BlackRaven, Sinclair; Discord ID 266967334478938114
+	["STEAM_0:1:99666895"] = "DuckRevoe", -- Discord ID 346077634494726144
+	["STEAM_0:1:99841604"] = "Astroux" -- Discord ID 769640326141968415
+}
